152e5f11a911c9cfe6022f365bdb7ce342747e0b
Afaandii <fandisantuy140@gmail.com>
2025-12-11 18:56:06 +0700

    adding merk,tipe,ukuran,warna infrastructure repository

---
+++ backend/internal/infrastructure/repository/merk_pg_repository.go
@@ -0,0 +1,69 @@
+package repository
+
+import (
+	"aplikasi-distro-zone-lsp-website/internal/domain/entities"
+	repo "aplikasi-distro-zone-lsp-website/internal/domain/repository"
+	"errors"
+
+	"gorm.io/gorm"
+)
+
+type merkPGRepository struct {
+	db *gorm.DB
+}
+
+func NewMerkPGRepository(db *gorm.DB) repo.MerkRepository {
+	return &merkPGRepository{db: db}
+}
+
+func (r *merkPGRepository) FindAll() ([]entities.Merk, error) {
+	var list []entities.Merk
+	if err := r.db.Order("id_merk").Find(&list).Error; err != nil {
+		return nil, err
+	}
+	return list, nil
+}
+
+func (r *merkPGRepository) FindByID(idMerk int) (*entities.Merk, error) {
+	var rol entities.Merk
+	if err := r.db.First(&rol, idMerk).Error; err != nil {
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			return nil, nil
+		}
+		return nil, err
+	}
+	return &rol, nil
+}
+
+func (r *merkPGRepository) Create(c *entities.Merk) error {
+	return r.db.Create(c).Error
+}
+
+func (r *merkPGRepository) Update(c *entities.Merk) error {
+	result := r.db.Model(&entities.Merk{}).
+		Where("id_merk = ?", c.IDMerk).
+		Updates(map[string]interface{}{
+			"nama_merk":  c.NamaMerk,
+			"keterangan": c.Keterangan,
+		})
+	if result.Error != nil {
+		return result.Error
+	}
+	if result.RowsAffected == 0 {
+		return errors.New("no rows updated")
+	}
+
+	return nil
+}
+
+func (r *merkPGRepository) Delete(idMerk int) error {
+	result := r.db.Delete(&entities.Merk{}, idMerk)
+	if result.Error != nil {
+		return result.Error
+	}
+
+	if result.RowsAffected == 0 {
+		return errors.New("no rows deleted")
+	}
+	return nil
+}
---
+++ backend/internal/infrastructure/repository/tipe_pg_repository.go
@@ -0,0 +1,69 @@
+package repository
+
+import (
+	"aplikasi-distro-zone-lsp-website/internal/domain/entities"
+	repo "aplikasi-distro-zone-lsp-website/internal/domain/repository"
+	"errors"
+
+	"gorm.io/gorm"
+)
+
+type tipePGRepository struct {
+	db *gorm.DB
+}
+
+func NewTipePGRepository(db *gorm.DB) repo.TipeRepository {
+	return &tipePGRepository{db: db}
+}
+
+func (r *tipePGRepository) FindAll() ([]entities.Tipe, error) {
+	var list []entities.Tipe
+	if err := r.db.Order("id_tipe").Find(&list).Error; err != nil {
+		return nil, err
+	}
+	return list, nil
+}
+
+func (r *tipePGRepository) FindByID(idTipe int) (*entities.Tipe, error) {
+	var rol entities.Tipe
+	if err := r.db.First(&rol, idTipe).Error; err != nil {
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			return nil, nil
+		}
+		return nil, err
+	}
+	return &rol, nil
+}
+
+func (r *tipePGRepository) Create(c *entities.Tipe) error {
+	return r.db.Create(c).Error
+}
+
+func (r *tipePGRepository) Update(c *entities.Tipe) error {
+	result := r.db.Model(&entities.Tipe{}).
+		Where("id_tipe = ?", c.IDTipe).
+		Updates(map[string]interface{}{
+			"nama_tipe":  c.NamaTipe,
+			"keterangan": c.Keterangan,
+		})
+	if result.Error != nil {
+		return result.Error
+	}
+	if result.RowsAffected == 0 {
+		return errors.New("no rows updated")
+	}
+
+	return nil
+}
+
+func (r *tipePGRepository) Delete(idTipe int) error {
+	result := r.db.Delete(&entities.Tipe{}, idTipe)
+	if result.Error != nil {
+		return result.Error
+	}
+
+	if result.RowsAffected == 0 {
+		return errors.New("no rows deleted")
+	}
+	return nil
+}
