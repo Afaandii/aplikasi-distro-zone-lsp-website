c1eee3262532bce9491845e3dcd1fb2a27e23ecf
Afaandii <fandisantuy140@gmail.com>
2025-12-13 00:35:54 +0700

    inisialisasi all entities table model and adding repository, usecase in tarif_pengiriman, jam_operasional

---
+++ backend/internal/domain/entities/chat_cs.go
@@ -0,0 +1,21 @@
+package entities
+
+import "time"
+
+type ChatCS struct {
+	IDPesan    int       `json:"id_pesan" gorm:"primaryKey;column:id_pesan;autoIncrement"`
+	IDPengirim int       `json:"id_pengirim" gorm:"column:id_pengirim;index"`
+	IDPenerima int       `json:"id_penerima" gorm:"column:id_penerima;index"`
+	IsiPesan   string    `json:"isi_pesan" gorm:"type:text"`
+	TipePesan  string    `json:"tipe_pesan" gorm:"type:varchar(50)"`
+	WaktuKirim time.Time `json:"waktu_kirim"`
+	CreatedAt  time.Time `json:"created_at"`
+	UpdatedAt  time.Time `json:"updated_at"`
+
+	Pengirim User `gorm:"foreignKey:IDPengirim;references:IDPenerima;constraint:OnUpdate:RESTRICT,OnDelete:CASCADE"`
+	Penerima User `gorm:"foreignKey:IDPenerima;references:IDPenerima;constraint:OnUpdate:RESTRICT,OnDelete:CASCADE"`
+}
+
+func (ChatCS) TableName() string {
+	return "chat_cs"
+}
---
+++ backend/internal/domain/entities/transaksi.go
@@ -0,0 +1,20 @@
+package entities
+
+import "time"
+
+type Transaksi struct {
+	IDTransaksi      int       `json:"id_transaksi" gorm:"primaryKey;column:id_transaksi;autoIncrement"`
+	IDUser           int       `json:"id_user" gorm:"column:id_user;not null"`
+	KodeTransaksi    string    `json:"kode_transaksi" gorm:"type:varchar(255)"`
+	Total            int       `json:"total" gorm:"type:int"`
+	MetodePembayaran string    `json:"metode_pembayaran" gorm:"type:varchar(255)"`
+	StatusTransaksi  string    `json:"status_transaksi" gorm:"type:varchar(255)"`
+	CreatedAt        time.Time `json:"created_at"`
+	UpdatedAt        time.Time `json:"updated_at"`
+
+	User User `gorm:"foreignKey:IDUser;references:IDUser;constraint:OnUpdate:RESTRICT,OnDelete:CASCADE"`
+}
+
+func (Transaksi) TableName() string {
+	return "transaksi"
+}
