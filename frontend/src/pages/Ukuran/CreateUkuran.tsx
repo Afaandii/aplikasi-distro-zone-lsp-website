b55ef1024da62f0a17c0ada82e63d9e4b9b9b215
Afaandii <fandisantuy140@gmail.com>
2025-12-16 17:54:32 +0700

    manage crud api ukuran in fe send data to backend

--- frontend/src/pages/Ukuran/CreateUkuran.tsx
+++ frontend/src/pages/Ukuran/CreateUkuran.tsx
@@ -2,12 +2,12 @@ import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import axios from "axios";
 
-export default function CreateJenis() {
+export default function CreateUkuran() {
   const [successMessage, setSuccessMessage] = useState<string | null>(null);
   const navigate = useNavigate();
   const [formData, setFormData] = useState({
-    type_name: "",
-    description: "",
+    nama_ukuran: "",
+    keterangan: "",
   });
 
   const getToken = () => {
@@ -22,14 +22,14 @@ export default function CreateJenis() {
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
 
-    const token = getToken()
+    const token = getToken();
 
     try {
       const response = await axios.post(
-        "http://localhost:8000/api/v1/create-type-product",
+        "http://localhost:8080/api/v1/ukuran",
         {
-          type_name: formData.type_name,
-          description: formData.description,
+          nama_ukuran: formData.nama_ukuran,
+          keterangan: formData.keterangan,
         },
         {
           headers: {
@@ -38,14 +38,17 @@ export default function CreateJenis() {
           },
         }
       );
-      
-      setFormData({ type_name: "", description: "" });
+
+      setFormData({
+        nama_ukuran: "",
+        keterangan: "",
+      });
       if (response.status === 201) {
-        setSuccessMessage("Kategori berhasil ditambahkan.");
-        navigate("/type");
+        setSuccessMessage("Ukuran berhasil ditambahkan.");
+        navigate("/ukuran");
       }
     } catch (error: any) {
-      console.error("Error creating category:", error);
+      console.error("Error creating ukuran:", error);
     }
   };
 
@@ -54,38 +57,38 @@ export default function CreateJenis() {
       {/* Header Section */}
       <section className="mb-6">
         <div className="flex items-center justify-between p-3 rounded-t-lg">
-          <h1 className="text-2xl font-bold text-white">Form Tambah Jenis</h1>
+          <h1 className="text-2xl font-bold text-white">Form Tambah Ukuran</h1>
         </div>
       </section>
 
       {/* Form Card */}
       <div className="bg-gray-800 rounded-lg shadow-lg overflow-hidden">
         {successMessage && (
-            <div className="mb-4 p-3 bg-green-600 text-white rounded-md flex items-center justify-between">
-              <span>{successMessage}</span>
-              <button
-                onClick={() => setSuccessMessage(null)}
-                className="ml-2 text-white hover:text-gray-200"
-              >
-                &times;
-              </button>
-            </div>
-          )}
+          <div className="mb-4 p-3 bg-green-600 text-white rounded-md flex items-center justify-between">
+            <span>{successMessage}</span>
+            <button
+              onClick={() => setSuccessMessage(null)}
+              className="ml-2 text-white hover:text-gray-200"
+            >
+              &times;
+            </button>
+          </div>
+        )}
         <div className="p-6">
           <form onSubmit={handleSubmit}>
-            {/* Nama jenis Field */}
+            {/* Nama ukuran Field */}
             <div className="mb-4">
               <label
-                htmlFor="type_name"
+                htmlFor="nama_ukuran"
                 className="block text-sm font-medium text-white mb-1"
               >
-                Nama Jenis
+                Nama Ukuran
               </label>
               <input
                 type="text"
-                id="type_name"
-                name="type_name"
-                value={formData.type_name}
+                id="nama_ukuran"
+                name="nama_ukuran"
+                value={formData.nama_ukuran}
                 onChange={handleChange}
                 placeholder="Masukan nama kategori"
                 className="w-full px-3 py-2 bg-gray-700 border border-gray-600 rounded-md text-white placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
@@ -93,19 +96,19 @@ export default function CreateJenis() {
               />
             </div>
 
-            {/* description jenis */}
+            {/* Keterangan ukuran */}
             <div className="mb-6">
               <label
-                htmlFor="description"
+                htmlFor="keterangan"
                 className="block text-sm font-medium text-white mb-1"
               >
                 Deskripsi
               </label>
               <input
                 type="text"
-                id="description"
-                name="description"
-                value={formData.description}
+                id="keterangan"
+                name="keterangan"
+                value={formData.keterangan}
                 onChange={handleChange}
                 placeholder="Masukan description"
                 className="w-full px-3 py-2 bg-gray-700 border border-gray-600 rounded-md text-white placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
@@ -122,7 +125,7 @@ export default function CreateJenis() {
                 Simpan
               </button>
               <Link
-                to="/type"
+                to="/ukuran"
                 className="inline-flex items-center px-4 py-2 bg-blue-500 hover:bg-blue-600 text-white font-medium rounded-md transition-colors duration-200"
               >
                 Kembali
--- frontend/src/pages/Ukuran/EditUkuran.tsx
+++ frontend/src/pages/Ukuran/EditUkuran.tsx
@@ -2,33 +2,36 @@ import { useEffect, useState } from "react";
 import { Link, useNavigate, useParams } from "react-router-dom";
 import axios from "axios";
 
-export default function EditJenis() {
-  const { id } = useParams<{ id: string }>();
+export default function EditUkuran() {
+  const { id_ukuran } = useParams<{ id_ukuran: string }>();
   const navigate = useNavigate();
   const [successMessage, setSuccessMessage] = useState<string | null>(null);
 
   const [formData, setFormData] = useState({
-    type_name: "", 
-    description: "",
+    nama_ukuran: "",
+    keterangan: "",
   });
 
   const getToken = () => {
     return localStorage.getItem("token") || sessionStorage.getItem("token");
   };
 
-  const fetchJenis = async () => {
+  const fetchUkuran = async () => {
     try {
-      const token = getToken()
-      const res = await axios.get(`http://localhost:8000/api/v1/edit-type-product/${id}`, {
-        headers: { Authorization: `Bearer ${token}` },
-      });
+      const token = getToken();
+      const res = await axios.get(
+        `http://localhost:8080/api/v1/ukuran/${id_ukuran}`,
+        {
+          headers: { Authorization: `Bearer ${token}` },
+        }
+      );
 
       // Cari kategori sesuai ID
-      const typ = res.data.datas
-      if (typ) {
+      const uk = res.data;
+      if (uk) {
         setFormData({
-          type_name: typ.type_name,
-          description: typ.description ?? "",
+          nama_ukuran: uk.nama_ukuran,
+          keterangan: uk.keterangan ?? "",
         });
       }
     } catch (err) {
@@ -37,20 +40,20 @@ export default function EditJenis() {
   };
 
   useEffect(() => {
-    fetchJenis();
+    fetchUkuran();
   }, []);
 
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
 
-    const token = getToken()
+    const token = getToken();
 
     try {
       const response = await axios.put(
-        `http://localhost:8000/api/v1/update-type-product/${id}`,
+        `http://localhost:8080/api/v1/ukuran/${id_ukuran}`,
         {
-          type_name: formData.type_name,
-          description: formData.description,
+          nama_ukuran: formData.nama_ukuran,
+          keterangan: formData.keterangan,
         },
         {
           headers: {
@@ -60,12 +63,12 @@ export default function EditJenis() {
         }
       );
 
-      if (response.status === 201) {
-        setSuccessMessage("Kategori berhasil diperbarui.");
-        navigate("/type");
+      if (response.status === 200) {
+        setSuccessMessage("Ukuran berhasil diperbarui.");
+        navigate("/ukuran");
       }
     } catch (error) {
-      console.error("Error updating category:", error);
+      console.error("Error updating ukuran:", error);
     }
   };
 
@@ -78,38 +81,38 @@ export default function EditJenis() {
     <>
       <section className="mb-6">
         <div className="flex items-center justify-between p-3 rounded-t-lg">
-          <h1 className="text-2xl font-bold text-white">Form Edit Jenis</h1>
+          <h1 className="text-2xl font-bold text-white">Form Edit Ukuran</h1>
         </div>
       </section>
 
       {/* Form Card */}
       <div className="bg-gray-800 rounded-lg shadow-lg overflow-hidden">
         {successMessage && (
-            <div className="mb-4 p-3 bg-green-600 text-white rounded-md flex items-center justify-between">
-              <span>{successMessage}</span>
-              <button
-                onClick={() => setSuccessMessage(null)}
-                className="ml-2 text-white hover:text-gray-200"
-              >
-                &times;
-              </button>
-            </div>
-          )}
+          <div className="mb-4 p-3 bg-green-600 text-white rounded-md flex items-center justify-between">
+            <span>{successMessage}</span>
+            <button
+              onClick={() => setSuccessMessage(null)}
+              className="ml-2 text-white hover:text-gray-200"
+            >
+              &times;
+            </button>
+          </div>
+        )}
         <div className="p-6">
           <form onSubmit={handleSubmit}>
-            {/* Nama jenis */}
+            {/* Nama ukuran */}
             <div className="mb-4">
               <label
-                htmlFor="name"
+                htmlFor="nama_ukuran"
                 className="block text-sm font-medium text-white mb-1"
               >
-                Nama Jenis
+                Nama Ukuran
               </label>
               <input
                 type="text"
-                id="name"
-                name="name"
-                value={formData.type_name}
+                id="nama_ukuran"
+                name="nama_ukuran"
+                value={formData.nama_ukuran}
                 onChange={handleChange}
                 placeholder="Masukan nama kategori"
                 className="w-full px-3 py-2 bg-gray-700 border border-gray-600 rounded-md text-white placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
@@ -117,19 +120,19 @@ export default function EditJenis() {
               />
             </div>
 
-            {/* description jenis */}
+            {/* Keterangan ukuran */}
             <div className="mb-6">
               <label
-                htmlFor="description"
+                htmlFor="keterangan"
                 className="block text-sm font-medium text-white mb-1"
               >
-                Deskripsi
+                Keterangan
               </label>
               <input
                 type="text"
-                id="description"
-                name="description"
-                value={formData.description}
+                id="keterangan"
+                name="keterangan"
+                value={formData.keterangan}
                 onChange={handleChange}
                 placeholder="Masukan description"
                 className="w-full px-3 py-2 bg-gray-700 border border-gray-600 rounded-md text-white placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
@@ -145,7 +148,7 @@ export default function EditJenis() {
                 Simpan
               </button>
               <Link
-                to="/type"
+                to="/ukuran"
                 className="inline-flex items-center px-4 py-2 bg-blue-500 hover:bg-blue-600 text-white font-medium rounded-md transition-colors duration-200"
               >
                 Kembali
